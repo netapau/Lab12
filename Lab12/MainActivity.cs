3aad929222c268448618183ed1409a64b4c9cf64
Tony Simoes <sr.tony@gmail.com>
2017-06-21 19:24:20 +0200

    Se debe Implementar la logica del MainActivity.cs

--- Lab12/MainActivity.cs
+++ Lab12/MainActivity.cs
@@ -12,7 +12,9 @@ namespace Lab12
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+
         }
     }
 }
--- Lab12/Resources/Resource.Designer.cs
+++ Lab12/Resources/Resource.Designer.cs
@@ -60,23 +60,23 @@ namespace Lab12
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050000
+			public const int ColorImageView = 2131034112;
+			
 			// aapt resource value: 0x7f050005
 			public const int ColorsListView = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int HexValueTextView = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int TitleTextView = 2131034114;
 			
 			// aapt resource value: 0x7f050004
 			public const int ValidationTextView = 2131034116;
 			
-			// aapt resource value: 0x7f050000
-			public const int imageView1 = 2131034112;
-			
 			// aapt resource value: 0x7f050001
 			public const int linearLayout1 = 2131034113;
-			
-			// aapt resource value: 0x7f050002
-			public const int textView3 = 2131034114;
-			
-			// aapt resource value: 0x7f050003
-			public const int textView4 = 2131034115;
 			
 			static Id()
 			{
